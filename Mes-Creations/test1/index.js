62c5d67c42ff1cbb7a49793db860caf94330eba3
SEVERINE-GARDOUX <severine0613pro@gmail.com>
2022-03-23 15:25:08 +0100

    2303

--- Mes-Creations/test1/index.js
+++ Mes-Creations/test1/index.js
@@ -1,10 +1,10 @@
 const color = document.querySelector ('.color')
-const couleur = document.querySelector ('.btn')
+const couleur = document.querySelector ('#btn')
 
 couleur.addEventListener ('click', function () {
     document.querySelector ('h2').textContent = ' Vous aimez la couleur ' + color.value 
     document.querySelector ('body').style.backgroundColor = color.value
-    document.querySelector ('.btn').style.backgroundColor = color.value
+    document.querySelector ('#btn').style.backgroundColor = color.value
     document.querySelector ('.contenu-img').style.backgroundColor = color.value
 })
 
@@ -15,20 +15,52 @@ image.addEventListener ('change', function (e) {
 }
 )
 
-//const son = document.querySelector ('#emoji')
 
-// son.onclick = function () {
-//     audio.play ()
-// }
+const bravo = document.querySelector ('.br')
+const bisous = document.querySelector ('.bis')
+const boo = document.querySelector ('.boo')
+const rire = document.querySelector ('.rire')
+const train = document.querySelector ('.train')
+const sonnette = document.querySelector ('.sonnette')
+const audio = document.querySelector ('audio')
+bravo.addEventListener ('click', function (e) {
+    console.log (e.target)
+    audio.src = e.target.dataset.audio
+    console.log (e.target.dataset.audio)
+    audio.play ()
+})
 
-const son = document.querySelector ('#emoji')
+bisous.addEventListener ('click', function (e) {
+    console.log (e.target)
+    audio.src = e.target.dataset.audio
+    console.log (e.target.dataset.audio)
+    audio.play ()
+})
 
-son.addEventListener ('click', function () {
+boo.addEventListener ('click', function (e) {
+    console.log (e.target)
+    audio.src = e.target.dataset.audio
+    console.log (e.target.dataset.audio)
+    audio.play ()
+})
 
-    if (value=bravo) {
-        audio.play (applaudissement.mp3)
-    }else if (value=bis) {
-        audio.play (kiss2.mp3)
-    }
-    
+rire.addEventListener ('click', function (e) {
+    console.log (e.target)
+    audio.src = e.target.dataset.audio
+    console.log (e.target.dataset.audio)
+    audio.play ()
+})
+
+train.addEventListener ('click', function (e) {
+    console.log (e.target)
+    audio.src = e.target.dataset.audio
+    console.log (e.target.dataset.audio)
+    audio.play ()
+})
+
+sonnette.addEventListener ('click', function (e) {
+    console.log (e.target)
+    audio.src = e.target.dataset.audio
+    console.log (e.target.dataset.audio)
+    audio.play ()
 })
